25f7d43e4281ab2931056dfd5cdd3bcda9d82274
FilippovaLena <lenchf96@gmail.com>
2025-01-06 04:56:26 +0300

    Доработала игру.

--- 01.js
+++ 01.js
@@ -1,21 +1,31 @@
-const location1 = 3
-const location2 = 4
-const location3 = 5
+const randomLoc = Math.floor(Math.random() * 5)
+const location1 = randomLoc
+const location2 = location1 + 1
+const location3 = location2 + 1
 
-let isSunk = false
-let guess
-let hits = 0
+let isSunk = false;
+let guess;
+let hits = 0;
+let guesses = 0;
 
 while (isSunk === false) {
-guess = +prompt("Fire! Enter a number 0-6")
-if (guess === location1 || guess === location2 || guess === location3){
-    alert("HIT!")
-    hits = hits + 1 
-    if(hits === 3) {
-        isSunk = true
-        alert("You sank my battleship!")
+    guess = +prompt("Fire! Enter a number 0-6")
+    if (guess < 0 || guess > 6) {
+        alert("Please, enter a valid cell number")
+    } else {
+        guesses = guesses + 1
+        if (guess === location1 || guess === location2 || guess === location3) {
+            alert("HIT!")
+            hits = hits + 1
+            if (hits === 3) {
+                isSunk = true
+                alert("You sank my battleship!")
+            }
+        } else {
+            alert("MISS!")
+        }
     }
-} else { 
-    alert("MISS!")
 }
-}  
+
+const stats = "You have " + hits + " hits out of " + guesses + " shots."
+alert(stats)
